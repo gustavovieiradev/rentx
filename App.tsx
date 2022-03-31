d4a7fb39a9ef9d735d55991f0afecb84b63eedcd
Gustavo Vieira <gustavo.vieira@vitta.me>
2022-03-31 11:45:23 -0300

    feat: global styles

--- App.tsx
+++ App.tsx
@@ -1,6 +1,32 @@
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Home from './src/screens/Home';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
