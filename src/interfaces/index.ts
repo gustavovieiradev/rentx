68277ca02df666be870ccb5223fb580e19280c65
Gustavo Vieira <gustavo.vieira@vitta.me>
2022-04-06 15:52:35 -0300

    feat: add navigation

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+import { StackScreenProps } from '@react-navigation/stack';
+
+export type RootStackParamList = {
+  Home: undefined;
+  CarDetails: undefined;
+  Scheduling: undefined;
+  SchedulingComplete: undefined;
+  SchedulingDetails: undefined;
+};
---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,24 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { RootStackParamList } from '../interfaces';
+import CarDetails from '../screens/CarDetails';
+import Home from '../screens/Home';
+import Scheduling from '../screens/Scheduling';
+import SchedulingComplete from '../screens/SchedulingComplete';
+import SchedulingDetails from '../screens/SchedulingDetails';
+
+const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
+
+const StackRoutes: React.FC = () => {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Home" component={Home} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="SchedulingComplete" component={SchedulingComplete} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+    </Navigator>
+  );
+};
+
+export default StackRoutes;
