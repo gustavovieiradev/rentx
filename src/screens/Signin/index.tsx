f11a68b6f3db3572b93c124645ea74b816a85c92
Gustavo Vieira <gustavo.vieira@vitta.me>
2022-04-10 16:56:28 -0300

    feat: validation form login

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Keyboard, KeyboardAvoidingView, StatusBar } from 'react-native';
+import { Alert, Keyboard, KeyboardAvoidingView, StatusBar } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useTheme } from 'styled-components';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PasswordInput from '../../components/PasswordInput';
+import * as Yup from 'yup';
 
 import { Container, Footer, Form, Header, SubTitle, Title } from './styles';
 
@@ -12,6 +13,29 @@ const Signin: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const theme = useTheme();
+
+  async function handleSignin() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha é obtrigatória'),
+      });
+
+      await schema.validate({ email, password });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', err.message);
+      }
+
+      return Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer o login'
+      );
+    }
+  }
+
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -54,7 +78,7 @@ const Signin: React.FC = () => {
           </Form>
 
           <Footer>
-            <Button title="Login" />
+            <Button title="Login" onPress={handleSignin} />
             <Button
               title="Criar conta gratuita"
               light
